f2b437a39cc3ac48a268b6d0fe182fa0444c510d
MingboPeng <mingo1214@gmail.com>
2021-01-05 09:44:37 -0500

    fix(handlers): combine handlers into one project

---
+++ src/Pollination.Handlers/Handler.cs
@@ -0,0 +1,62 @@
+﻿using HoneybeeRhino.Entity;
+using System;
+using System.IO;
+
+// project name has to be the format of NameSpace.Handlers. 
+// For example: if the name space is "HoneybeeSchema", then project name should be HoneybeeSchema.Handlers.
+namespace Pollination
+{
+    public static class Handlers
+    {
+        /// <summary>
+        /// Translate Honeybee Model to HBJson and return the saved file path
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static string RhinoHBModelToJSON(object param)
+        {
+            var model = ModelEntityTable.Instance.CurrentModelEntity;
+            var hbObj = model.GetHBModel();
+
+            if (hbObj == null) throw new ArgumentNullException("Input model");
+            var temp = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".hbjson");
+            using (var outputFile = new StreamWriter(temp))
+            {
+                var json = hbObj.ToJson();
+                outputFile.Write(json);
+            }
+
+            if (!File.Exists(temp)) throw new ArgumentException($"Failed to save the model to {temp}");
+            return temp;
+        }
+
+        /// <summary>
+        /// Translate Honeybee Model to HBJson and return the saved file path
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public static string HBModelToJSON(object param)
+        {
+            if (param == null) throw new ArgumentNullException("Input model: null is not valid");
+
+            if (param is string s)
+                return s;
+
+            var model = param as HoneybeeSchema.Model;
+            if (model == null) throw new ArgumentNullException("Input is not a Honeybee Model");
+
+
+            var temp = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + ".hbjson");
+            using (var outputFile = new StreamWriter(temp))
+            {
+                var json = model.ToJson();
+                outputFile.Write(json);
+            }
+
+            if (!File.Exists(temp)) throw new ArgumentException($"Failed to save the model to {temp}");
+            return temp;
+        }
+    }
+
+    
+}
