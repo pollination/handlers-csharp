e35cbebdbcc5f6ab951c1b58b7d96c3a6d92bedd
MingboPeng <mingo1214@gmail.com>
2021-01-20 15:07:20 -0500

    chore(CD): release to nuget

---
+++ .github/workflows/update_version.py
@@ -0,0 +1,24 @@
+import os
+import re
+import sys
+
+
+working_dir = os.getcwd()
+
+new_version = sys.argv[1]
+print(f'New version: \n{new_version}\n')
+
+
+# checking files
+def replaceVersion(file, new_text):
+    with open(file, encoding='utf-8', mode='r') as csFile:
+        s = csFile.read()
+    with open(file, encoding='utf-8', mode='w') as f:
+        regex = r"(?<=\<Version\>)\S+(?=\<\/Version\>)"
+        s = re.sub(regex, new_text, s)
+        print(f"Update version in {file} to: {new_text}")
+        f.write(s)
+
+
+config = os.path.join(working_dir, 'src', 'Pollination.Handlers', 'Pollination.Handlers.csproj')
+replaceVersion(config, str(new_version))
